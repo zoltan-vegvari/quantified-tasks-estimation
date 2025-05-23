a8c9e8d3852d0883d2ab9ebea1293a1e4267943a
Zoltan Vegvari <14219356+zoltan-vegvari@users.noreply.github.com>
2025-05-23 21:52:53 +0200

    Add RouterModule import to App component

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { provideRouter, RouterOutlet } from '@angular/router';
 import { routes } from './app/app.routes';
 import { HomeComponent } from './app/components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterOutlet],
   template: `<router-outlet></router-outlet>`
 })
 export class App {}
